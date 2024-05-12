f8e7a4aa0a010aa873d4bc113e984596ff949d36
ArunavaCoderEm <acecankill@gmail.com>
2024-05-12 20:30:28 +0530

    nav final fixes to taskease

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -2,22 +2,20 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
-  const [isSearchOpen, setIsSearchOpen] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [menu,setMenu] = useState('home');
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
-    setIsSearchOpen(false);
   };
 
   return (
     <>
-      <nav className="bg-gray-950">
+      <nav className="backdrop-blur-lg bg-white/20">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse" onClick={() => {setMenu('home')}}>
+          <Link to="/" className="flex ml-10 items-center space-x-3 rtl:space-x-reverse" onClick={() => {setMenu('home')}}>
             <img src="./todo.png" className="h-8 rounded-full"/>
-            <span className="self-center text-xl font-mono font-semibold whitespace-nowrap hov dark:text-white"><span className='text-blue-500 font-serif font-bold'>T</span>ask<span className='text-blue-500 font-serif font-bold'>E</span>ase</span>
+            <span className="self-center text-xl font-mono font-semibold whitespace-nowrap hov dark:text-white"><span className='text-violet-500 font-serif font-bold'>T</span>ask<span className='text-violet-500 font-serif font-bold'>E</span>ase</span>
           </Link>
           <div className="flex md:order-2">
             
@@ -28,30 +26,17 @@ export default function Navbar() {
               </svg>
             </button>
           </div>
-          <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isSearchOpen ? 'block' : 'hidden'}`} id="navbar-search">
-            <div className="relative mt-3 md:hidden">
-              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                </svg>
-              </div>
-            </div>
-          </div>
-          <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isMenuOpen ? 'block' : 'hidden'}`}>
+          <div className={`m-1 justify-end items-end w-full md:flex md:w-auto md:order-1 ${isMenuOpen ? 'block' : 'hidden'}`}>
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium text-center rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
               <li>
-                <Link to="/" className={`block p-2 font-sm rounded-md hover:text-amber-500 transition-all duration-300 ${(menu == 'home'? `bg-gradient-to-t from-violet-600 to-violet-950 text-white` : `text-white`)}`} onClick={() => {setMenu('home')}} aria-current="page">Home</Link>
+                <Link to="/" className={`block p-2 font-sm rounded-md  hover:border-gray-700 border-transparent border-2 hover:border-current hover:text-violet-300 transition-all duration-300 ${(menu == 'home'? `bg-gradient-to-t from-violet-600 to-violet-950 text-white` : `text-white`)}`} onClick={() => {setMenu('home')}} aria-current="page">Home</Link>
               </li>
               <li>
-                <Link to="/about"  className={`block p-2 font-sm rounded-md hover:text-amber-500 transition-all duration-300 ${(menu == 'about'? `bg-gradient-to-t from-violet-600 to-violet-950 text-white` : `text-white`)}`} onClick={() => {setMenu('about')}}>About Me</Link>
+                <Link to="/about"  className={`block p-2 font-sm rounded-md  hover:border-gray-700 border-transparent border-2 hover:border-current hover:text-violet-300 transition-all duration-300 ${(menu == 'about'? `bg-gradient-to-t from-violet-600 to-violet-950 text-white` : `text-white`)}`} onClick={() => {setMenu('about')}}>About Me</Link>
               </li>
               <li>
-                <Link to="/features"  className={`block p-2 font-sm rounded-md hover:text-amber-500 transition-all duration-300 ${(menu == 'feature'? `bg-gradient-to-t from-violet-600 to-violet-950 text-white` : `text-white`)}`} onClick={() => {setMenu('feature')}}>Features</Link>
-              </li>
-              <li>
-                <Link to="/signup"  className={`block p-2 font-sm rounded-md hover:text-amber-500 transition-all duration-300 bg-gradient-to-b from-violet-600 to-violet-950 text-white`} onClick={() => {setMenu('')}}>Sign Up</Link>
-              </li>
-              
+                <Link to="/features"  className={`block p-2 font-sm rounded-md  hover:border-gray-700 border-transparent border-2 hover:border-current hover:text-violet-300 transition-all duration-300 ${(menu == 'feature'? `bg-gradient-to-t from-violet-600 to-violet-950 text-white` : `text-white`)}`} onClick={() => {setMenu('feature')}}>Features</Link>
+              </li>              
             </ul>
           </div>
         </div>
