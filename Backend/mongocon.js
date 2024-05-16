27684887c921878d39f288c587aa22f748fea447
ArunavaCoderEm <acecankill@gmail.com>
2024-05-16 21:22:46 +0530

    push to cehck

--- Backend/mongocon.js
+++ Backend/mongocon.js
@@ -3,7 +3,7 @@ require('dotenv/config');
 
 async function mongocon (req, res) {
     try {
-        await mongo.connect(process.env.MONGO_CONN_STRING);
+        await mongo.connect("mongodb+srv://godard:godard06mongo@cluster0.ymoraxq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
         console.log('DB connection established');
     } catch (e) {
         res.status(400).json({
