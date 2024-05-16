1f2ed8ff320f9503dc9ab66d4edee8f468cff3de
ArunavaCoderEm <acecankill@gmail.com>
2024-05-16 19:13:24 +0530

    push this

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react()
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://taskeaseserver.vercel.app/', 
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/users/, '')
+      }
+    }
+  }
+});
+
+
