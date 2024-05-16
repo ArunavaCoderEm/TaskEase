862b99a621894c64a767fb7e393423f1bf545752
ArunavaCoderEm <acecankill@gmail.com>
2024-05-16 20:57:18 +0530

    pushes and checks

--- Backend/index.js
+++ Backend/index.js
@@ -9,24 +9,28 @@ const PORT = 7070;
 
 require('dotenv/config');
 
+mongocon();
+
 server.use(express.json())
 
-server.use(cors());
+server.use(cors({
+    origin:  ["https://taskeaseserver.vercel.app/", "http://localhost:5173" ,"https://taskease-kappa.vercel.app"],
+    methods: ['POST', 'DELETE', 'GET', 'PUT', 'PATCH'],
+    credentials: true
+}));
+
+server.options("" ,cors({
+    origin:  ["https://taskeaseserver.vercel.app/", "http://localhost:5173" , "https://taskease-kappa.vercel.app"],
+    methods: ['POST', 'DELETE', 'GET', 'PUT', 'PATCH'],
+    credentials: true
+}) )
 
 server.get('/',(req,res) => {
     res.send("Server started");
 })
 
-mongocon();
-
-
 server.use(express.static("public"));
 server.use(express.urlencoded({extended:true}));
-
-
-server.use("/users/reglog",authchecker);
-server.use("/users/data",process);
-
 
 server.listen(PORT, () => {
     console.log("Server started successfully at ",PORT);
