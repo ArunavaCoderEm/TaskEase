3034f5971dfcdcfe000d9bf2627d411ab62bc436
ArunavaCoderEm <acecankill@gmail.com>
2024-05-17 13:21:33 +0530

    check to vercel

--- Frontend/src/Components/Alert.tsx
+++ Frontend/src/Components/Alert.tsx
@@ -4,7 +4,7 @@ export default function Alert(props:any) {
   return (
     <div className="bg-red-100 text-center border m-3 border-blue-400 text-blue-700 px-4 py-3 rounded relative" role="alert">
   <strong className="font-bold mx-3">{props.title}</strong>
-  <span className="block sm:inline">{props.desc}</span> 
+  <span className="block sm:inline">{props.desc}</span>  
 </div>
   )
 }
